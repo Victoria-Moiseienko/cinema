7a85d07d19fc989ec2ea19597cb4123176663a99
Victoria-Moiseienko <teasevic@gmail.com>
2020-09-30 17:37:12 +0300

    update after student review: delete setId()

--- src/main/java/vic/cinema/Main.java
+++ src/main/java/vic/cinema/Main.java
@@ -5,7 +5,7 @@ import vic.cinema.model.Movie;
 import vic.cinema.service.MovieService;
 
 public class Main {
-    private static Injector injector = Injector.getInstance("vic/cinema");
+    private static Injector injector = Injector.getInstance("vic.cinema");
 
     public static void main(String[] args) {
         Movie movie = new Movie();
